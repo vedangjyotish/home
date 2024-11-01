17bf1482ec855922b9b9096a2b07dbd396fa180a
barungh <barungh@hotmail.com>
2024-11-01 07:12:19 +0530

    appointment page adjusted for laptop screen

--- src/app/appointment/appointment.component.ts
+++ src/app/appointment/appointment.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-appointment',
@@ -8,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './appointment.component.css'
 })
 export class AppointmentComponent {
+  imageUrl: string = '../../assets/appointment/girl.png';
+  textAreaRows: number = 8;
+  textAreaCols: number = 31;
 
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.breakpointObserver.observe([
+      '(max-width: 1440px)'
+    ]).subscribe(result => {
+      // console.log('Width:', window.innerWidth);
+      if (result.matches) {
+        this.imageUrl = '../../assets/appointment/girl_med.png';
+        this.textAreaRows = 4;
+      }
+    });
+  }
 }
