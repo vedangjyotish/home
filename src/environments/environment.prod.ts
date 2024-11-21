c77fb7f235a4758f834936edd709c32f1e47c373
barungh <barungh@hotmail.com>
2024-11-21 20:36:13 +0530

    Don't know what I did

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.vedang.com/api'  // Production API URL - Replace with your actual production server URL before deployment
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.0.102:8000/api'  // Django API URL
+  apiUrl: 'http://192.168.0.102:8000/api'  // Development API URL - Local Django server
 };
